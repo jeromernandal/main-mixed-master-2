d87d320d097ee08e4fdae11a2436f6d4cd53b1b7
JR <103070673+jeromernandal@users.noreply.github.com>
2023-04-05 14:11:44 -0700

    all

---
+++ mixedmfers-main-mixed/dist/contractAddress.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var contractAddress = "0x58814F29a0a93dE46342502C3112d468BF92585a";
+var _default = contractAddress;
+exports["default"] = _default;
---
+++ mixedmfers-main-mixed/src/contractAddress.js
@@ -0,0 +1,2 @@
+const contractAddress = "0x58814F29a0a93dE46342502C3112d468BF92585a";
+export default contractAddress;
